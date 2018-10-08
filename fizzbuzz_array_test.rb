437d4831672d9951bacd50ecf87ef64a88201a06
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-08 11:07:48 -0400

    test number 24

--- fizzbuzz_array_test.rb
+++ fizzbuzz_array_test.rb
@@ -6,7 +6,9 @@ class Testing < Minitest::Test
 		assert_equal(Array, fizz_array.class)
 	end
 	def test_number_5
-		assert_equal("Mined Minds ", fizz(5,fizz_array))
+		assert_equal(["Minds"], fizz(5,fizz_array))
 	end
-
+	def test_number_24
+		assert_equal(["Mined"], fizz(24,fizz_array))
+	end
 end
