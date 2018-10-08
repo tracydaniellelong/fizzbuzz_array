6c6d3bade763136d78cac35ee70d52baa8603e59
tracydaniellelong <tracydaniellelong@gmail.com>
2018-10-08 09:43:51 -0400

    test number 5

--- fizzbuzz_array_test.rb
+++ fizzbuzz_array_test.rb
@@ -5,8 +5,8 @@ class Testing < Minitest::Test
 	def test_array
 		assert_equal(Array, fizz_array.class)
 	end
-
-
-
+	def test_number_5
+		assert_equal("Mined Minds ", fizz(5,fizz_array))
+	end
 
 end
